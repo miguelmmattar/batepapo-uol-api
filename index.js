b41c8a9cd975fa42cdb8f97fe9aefef59f3edebb
Miguel Faria <mattar1704miguel@gmail.com>
2022-09-03 11:49:11 -0300

    POST/participants implemented

--- index.js
+++ index.js
@@ -2,31 +2,71 @@ import express from 'express';
 import cors from 'cors';
 import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
+import joi from 'joi';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const participantsSchema = joi.object({
+    name: joi.string().empty().required()
+});
+
 const app = express();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect(() => {
+    db = mongoClient.db('bate_papo_uol');
+});
 
 app.use(cors());
 app.use(express.json());
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 //essa parte vai mudar para usar o mongo
 const participants = [];
 const messages = [];
 
-app.post('/participants', (req, res) => {
+app.post('/participants', async (req, res) => {
     const { name } = req.body;
 
-    if(!name || name === "") {
-        res.status(422).send({message: 'O campo deve ser preenchido com informações válidas!'});
-        return;
-    }
-    
-    //falta requisitos
+    try {
+        const response = await db
+            .collection('participants')
+            .findOne({name: name});
 
-    res.sendStatus(201);
+        if(response) {
+            res.status(409).send('Este nome já existe! Tente outro.');
+            return;
+        }
+
+        const validation = participantsSchema.validate(req.body, { abortEarly: false });
+
+        if(validation.error) {
+            const errors = validation.error.details.map(detail => detail.message);
+            res.status(422).send(errors);
+            return;
+        }
+
+        await db
+            .collection('participants')
+            .insertOne({
+                name,
+                lastStatus: Date.now()
+            });
+
+        await db
+            .collection('messages')
+            .insertOne({
+                from: name, 
+                to: 'Todos', 
+                text: 'entra na sala...', 
+                type: 'status', 
+                time: dayjs().format('hh:mm:ss')
+            });
+
+        res.sendStatus(201);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.get('/participants', (req, res) => {
