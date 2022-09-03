b4ce4808d4f5c3c5c7903fca6cd68169d9dbd127
Miguel Faria <mattar1704miguel@gmail.com>
2022-09-03 13:12:04 -0300

    POST/status implemented

--- index.js
+++ index.js
@@ -149,18 +149,30 @@ app.get('/messages', async (req, res) => {
     }   
 });
 
-app.post('/status', (req, res) => {
-    const { user } = req.headers;
-    const i = participants.indexOf(participants.find(participant => participant.name === user));
+app.post('/status', async (req, res) => {
+    const  user  = req.headers.user;
+    
+    try {
+        const response = await db
+            .collection('participants')
+            .findOne({ name: user });
 
-    if(i === -1) {
-        res.sendStatus(404);
-        return;
-    }
+        if(!response) {
+            res.sendStatus(404);
+            return;
+        }
 
-    participants[i].lastStaus = Date.now();
+        await db 
+            .collection('participants')
+            .updateOne({ name: user }, { $set: {
+                lastStatus: Date.now()
+            } 
+        });
 
-    res.sendStatus(200);
+        res.sendStatus(200);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }   
 });
 
 setInterval(() => {
